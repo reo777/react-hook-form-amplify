7cd208fecb4c69ebd8cc149531e053f2c367268b
reo777 <r.ishiyama@gemcook.com>
2020-01-19 18:55:28 +0900

    ### add 1000 forms

--- src/templates/Form/Main.tsx
+++ src/templates/Form/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Input, Button} from 'semantic-ui-react';
 import {useForm, Controller} from 'react-hook-form';
+import faker from 'faker';
 
 import * as handlers from './handlers';
 
@@ -11,18 +12,29 @@ const defaultValues = {main: ''};
 const Form = () => {
   const {handleSubmit, control, reset} = useForm({defaultValues});
 
+  const forms = [];
+  for (let index = 0; index < 1000; index++) {
+    forms.push(faker.name.firstName());
+  }
+
   return (
     <div className="main">
       <h1>Form</h1>
       <div className="w__main">
         <form onSubmit={handleSubmit(handlers.submit)}>
           {/* SemanticUIReactはControlledコンポーネントを使用する為、Controllerでラップする */}
-          <Controller
-            as={<Input />}
-            name="main"
-            control={control}
-            defaultValue=""
-          />
+          {forms.map((form: any, index: number) => {
+            return (
+              <div key={index}>
+                <Controller
+                  as={<Input />}
+                  name={faker.name.firstName()}
+                  control={control}
+                  defaultValue=""
+                />
+              </div>
+            );
+          })}
           <Button type="submit">Submit</Button>
           <button
             type="button"
--- src/templates/Form/handlers.ts
+++ src/templates/Form/handlers.ts
@@ -1,6 +1,5 @@
 import {delay} from 'blend-promise-utils';
 
 export const submit = async (value: any, e: any) => {
-  await delay(350);
   window.alert(JSON.stringify(value));
 };
